f0d8b25b8cec308ed1ba1320fda004d8fe61b1a8
juliomiguel1997@gmail.com <juliomiguel1997@gmail.com>
2022-07-04 12:29:45 -0300

    Add 'CORS' dependence in basic-api-default

--- basic-api-default/src/server.ts
+++ basic-api-default/src/server.ts
@@ -1,23 +1,30 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import apiRoutes from "../src/routes/apiRoutes"
+import cors from "cors";
+
+import apiRoutes from "../src/routes/apiRoutes";
 
 
 //--Configurando variaveis de ambiente#
 dotenv.config();
-//--Criando servidor
+//--Criando servidor#
 const server = express();
-//--Requisições com paramentros
+//--Criando filtro de acesso CORS#
+server.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT']
+}));
+//--Requisições com paramentros#
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(express.urlencoded({extended : true}));
-//--Acessando rotas
+//--Acessando rotas#
 server.use(apiRoutes);
-//--Tratando rotas invalidas
+//--Tratando rotas invalidas#
 server.use((req: Request, res: Response)=>{
     res.status(404);
     res.json({error: "Endpoint not found!"})
 });
-//--Indicando a porta do servidor
+//--Indicando a porta do servidor#
 server.listen(process.env.SERVER_PORT)
